2f0ba04d4d33c091eebbb6e9ca3e8985c9653fb6
Arthur Bricq <arthur.bricq@bluebotics.com>
2024-04-09 10:11:37 +0200

    Moves container: WIP engine. It seems to be quite good.

--- src/model/engine.rs
+++ src/model/engine.rs
@@ -3,6 +3,7 @@ use super::super::model::game::*;
 use super::super::model::moves::*;
 use std::time::Instant;
 use crate::model::engine::MoveOrderState::{AcceptsOnlyCapture, Finished, AcceptsAllMove};
+use crate::model::moves_container::{MovesContainer, SortedMovesContainer};
 
 enum MoveOrderState {
     AcceptsOnlyCapture,
@@ -38,8 +39,8 @@ pub struct Engine {
     depth: usize, 
     extra_depth: usize,
     iter: u64,
-    transposition_table: HashMap<ChessGame, ScoreType>,
     use_transposition: bool,
+    transposition_table: HashMap<ChessGame, ScoreType>,
 }
 
 impl Engine {
@@ -56,10 +57,15 @@ impl Engine {
     /// The function also returns the NPS (nodes per second) in the unit k-nps (for benchmarking)
     pub fn find_best_move(&mut self, game: ChessGame, white_to_play: bool) -> (Option<Move>, u128) {
         self.iter = 0;
+        
         self.transposition_table.clear();
+        
+        
         let start = Instant::now();
         let result = self.tree_search(game, white_to_play, 0, ScoreType::MIN, ScoreType::MAX, false);
         let end = start.elapsed().as_millis() as f64 / 1000.;
+        
+        
         let nps = (self.iter as f64) / end;
         println!("\n\nSolver finished after evaluating {} positions", self.iter);
         println!("    score = {} [points]", result.score);
@@ -120,104 +126,74 @@ impl Engine {
         };
 
         // get the list of available moves
-        // this is the only time that the function is called
-        let moves = game.get_available_moves(white_to_play);
-        
+        let mut container = SortedMovesContainer::new();
+        game.update_move_container(&mut container, white_to_play);
+
         // The best move is initialized with the first one
-        let mut best_move = 0;
+        let mut best_move = None;
+        
+        while container.has_next() {
+            let m = container.get_next();
 
-        // A small state machine is used to pass through moves in a specific order
-        // This is why the code is quite hard to read. 
-        // This state machine allows to avoid sorting moves, but I am not sure if this was a good decision.
-        // Instead of sorting moves, the `move_index` passes through the all list of moves several times and each time only 
-        // one type of move is accepted.
-        let mut move_index = 0;
-        let mut move_state = AcceptsOnlyCapture;
-        let mut visited_moves: HashSet<usize> = HashSet::with_capacity(moves.len());
-
-        loop {
-            if !visited_moves.contains(&move_index) && move_state.accepts(&moves[move_index]) {
-                visited_moves.insert(move_index);
-
-                if depth == 0 {
-                    println!("move = {:?}", moves[move_index]);
-                }
-
-                // TODO maybe these two functions can be squashed into a single one
-                let mut new_game = game.clone();
-                new_game.apply_move_unsafe(&moves[move_index]);
-
-                // call the recursion
-                let result = self.tree_search(new_game,
-                                              !white_to_play,
-                                              depth + 1,
-                                              alpha,
-                                              beta,
-                                              moves[move_index].is_capture());
-
-                if depth == 0 {
-                    println!("   score = {:?}, move = {:?}", result.score, result.best_move);
-                }
-
-                // Alpha beta pruning
-                if white_to_play {
-                    // Keep the maximum score
-                    if result.score > current_score {
-                        best_move = move_index;
-                        current_score = result.score;
-                    }
-                    // beta cutoff
-                    if current_score > beta {
-                        break;
-                    }
-                    // Update alpha: eg, the minimum score that white is guaranteed
-                    // alpha becomes the max of the score
-                    if result.score > alpha {
-                        alpha = result.score;
-                    }
-                } else {
-                    if result.score < current_score {
-                        best_move = move_index;
-                        current_score = result.score;
-                    }
-                    // alpha cutoff
-                    if current_score < alpha {
-                        break;
-                    }
-                    if result.score < beta {
-                        beta = result.score;
-                    }
-                }
+            if depth == 0 {
+                println!("move = {:?}", m);
             }
 
-            // Break when we have gone through all moves
-            if visited_moves.len() == moves.len() {
-                break;
+            // TODO maybe these two functions can be squashed into a single one
+            let mut new_game = game.clone();
+            new_game.apply_move_unsafe(&m);
+
+            // call the recursion
+            let result = self.tree_search(new_game,
+                                          !white_to_play,
+                                          depth + 1,
+                                          alpha,
+                                          beta,
+                                          m.is_capture());
+
+            if depth == 0 {
+                println!("   score = {:?}, move = {:?}", result.score, result.best_move);
             }
 
-            // When the move index reaches the end of the list of moves, apply the following
-            // state of the engine state machine
-            move_index += 1;
-            if move_index == moves.len() {
-                move_state.next();
-                match move_state {
-                    AcceptsOnlyCapture | AcceptsAllMove => move_index = 0,
-                    Finished => break
+            // Alpha beta pruning
+            if white_to_play {
+                // Keep the maximum score
+                if result.score > current_score {
+                    best_move = Some(m);
+                    current_score = result.score;
+                }
+                // beta cutoff
+                if current_score > beta {
+                    break;
+                }
+                // Update alpha: eg, the minimum score that white is guaranteed
+                // alpha becomes the max of the score
+                if result.score > alpha {
+                    alpha = result.score;
+                }
+            } else {
+                if result.score < current_score {
+                    best_move = Some(m);
+                    current_score = result.score;
+                }
+                // alpha cutoff
+                if current_score < alpha {
+                    break;
+                }
+                if result.score < beta {
+                    beta = result.score;
                 }
             }
+            
         }
+
 
         // Once we reach this point, we have explored all the possible moves of this branch
         // ==> we know which is the best move
         return SearchResult {
             score: current_score, 
-            best_move: Some(moves[best_move]),
+            best_move,
         };
 
-        // return if depth == 0 {
-        //     (current_score, Some(moves[best_move]))
-        // } else {
-        //     (current_score, None)
-        // };
     }
 }
